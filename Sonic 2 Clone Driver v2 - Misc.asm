667ecc7af1522a4da2f70e5b04e7e6a93ac541f1
Clownacy <Clownacy@users.noreply.github.com>
2016-05-11 21:39:10 +0100

    One-line cleanup

--- Sonic 2 Clone Driver v2 - Misc.asm
+++ Sonic 2 Clone Driver v2 - Misc.asm
@@ -1,4 +1,4 @@
 	include "sound/Sonic 2 Clone Driver v2 - Compatibility.asm"
 	include "sound/Sonic 2 Clone Driver v2 - Sound IDs.asm"
-	include	"sound/Sonic 2 Clone Driver v2 - RAM.asm"
+	include "sound/Sonic 2 Clone Driver v2 - RAM.asm"
 	include "sound/_smps2asm_inc.asm"
