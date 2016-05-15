bf7c14e593085e6e95e3692c8d47272937b16edb
Clownacy <Clownacy@users.noreply.github.com>
2016-05-15 13:27:59 +0100

    Fixed S2 compatibility

--- Sonic 2 files/Sonic 2 Clone Driver v2 - Compatibility.asm
+++ Sonic 2 files/Sonic 2 Clone Driver v2 - Compatibility.asm
@@ -1,10 +1,9 @@
 ; ========SONIC 2 GIT EQUATES========
 
-; ---MACROS---
-waitZ80 macro
-	; Dummy. Just here for S1 compatibility.
-	; stopZ80 already waits for the Z80.
-    endm
+PlayMusic = SMPS_PlayMusic
+PlaySoundLocal = SMPS_PlaySoundLocal
+PlaySound = SMPS_PlaySound
+PlaySoundStereo = SMPS_PlaySound2
 
 ; ---EQUATES---
 ; Z80 addresses
@@ -15,9 +14,6 @@ ym2612_a0 = 			$A04000
 ym2612_d0 = 			$A04001
 ym2612_a1 = 			$A04002
 ym2612_d1 = 			$A04003
-
-PlayMusic = SMPS_PlayMusic
-PlaySound = SMPS_PlaySound
 
 GloopBehaviour	= 1
 ;	| If 1, SndID_Gloop only plays on every other call.
