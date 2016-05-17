318118bbf2b9b6e8ba58a0a6152c82b2f1456268
Clownacy <Clownacy@users.noreply.github.com>
2016-05-17 17:32:20 +0100

    Compatibility file cleanup

--- Sonic 1 files/Sonic 2 Clone Driver v2 - Compatibility.asm
+++ Sonic 1 files/Sonic 2 Clone Driver v2 - Compatibility.asm
@@ -1,66 +1,58 @@
 ; ========SONIC 1 GIT EQUATES========
+SoundDriverLoad		= SMPS_LoadDACDriver
 
-SoundDriverLoad =	SMPS_LoadDACDriver
+PlaySound		= SMPS_PlayMusic
+PlaySound_Special	= SMPS_PlaySound
 
-PlaySound =		SMPS_PlayMusic
-PlaySound_Special =	SMPS_PlaySound
-
-; ---BOUNDARIES---
-MusID__First	= bgm__First
+; ---SOUND ID BOUNDARIES---
+MusID__First		= bgm__First
 ;	| ID of your first song
 ;
-MusID__End	= bgm__Last+1
+MusID__End		= bgm__Last+1
 ;	| ID of your last song+1
 ;
-SndID__First	= sfx__First
+SndID__First		= sfx__First
 ;	| ID of your first SFX
 ;
-SndID__End	= sfx__Last+1
+SndID__End		= sfx__Last+1
 ;	| ID of your last SFX+1
 ;
-SpecID__First	= spec__First
+SpecID__First		= spec__First
 ;	| ID of your first Special SFX
 ;
-SpecID__End	= spec__Last+1
+SpecID__End		= spec__Last+1
 ;	| ID of your last Special SFX+1
 ;
-FlgID__First	= flg__First
+FlgID__First		= flg__First
 ;	| ID of your first command
 ;
-FlgID__End	= flg__Last+1
+FlgID__End		= flg__Last+1
 ;	| ID of your last command+1
 ;
 
-; ---MUSIC---
-MusID_ExtraLife	= bgm_ExtraLife
+; ---MUSIC CONSTANTS---
+MusID_ExtraLife		= bgm_ExtraLife
 ;	| ID of your Extra Life jingle
 ;
 
-; ---SFX---
-SndID_Ring	= sfx_Ring
+; ---SFX CONSTANTS---
+SndID_Ring		= sfx_Ring
 ;	| ID of your ring SFX
 ;
-SndID_RingLeft	= sfx_RingLeft
+SndID_RingLeft		= sfx_RingLeft
 ;	| ID of your alternate ring SFX
 ;
-SndID_SpindashRev = $00
+SndID_SpindashRev	= $00
 ;	| Set this to the ID of your Spin Dash SFX (if you have one). Use with EnableSpinDashSFX
 ;
 
-; ---EQUATES---
+; ---DISASM-DEPENDANT VARIABLES AND FUNCTIONS---
 Clone_Driver_RAM	= (-(v_snddriver_ram&$80000000)<<1)|v_snddriver_ram
-;	| Just an equate
-;
-Graphics_Flags	= v_megadrive
-;	| Just an equate
-;
-Ctrl_1 = v_jpadhold1
-;	| Just an equate
-;
-Joypad_Read = ReadJoypads.read
-;	| Post-Hivebrain had the wonderful idea of removing a useful label. Luckily, AS lets us do this
-;
+Graphics_Flags		= v_megadrive
+Ctrl_1			= v_jpadhold1
+Joypad_Read		= ReadJoypads.read
 
+; ---FLAGS---
 SMPS_GloopSFXBehaviour	= 0
 ;	| If 1, SndID_Gloop only plays on every other call.
 ;
@@ -70,14 +62,14 @@ SMPS_PushSFXBehaviour	= 1
 SMPS_EnableSpecSFX	= 1
 ;	| If 1, include the Special SFX system. Used by S1's waterfall SFX
 ;
-SMPS_EnableSpinDashSFX = 0
+SMPS_EnableSpinDashSFX	= 0
 ;	| If 1, include S2's spin dash SFX pitch system
 ;
 SMPS_EnableContSFX	= 0
 ;	| If 1, include S3K's continuous SFX system
 ;
 SMPS_First_ContSFX	= $BC
-;	| Set this to the sound ID of your first continous SFX. This ID must be a higher number than any of your normal SFXes
+;	| Set this to the sound ID of your first continuous SFX. This ID must be a higher number than any of your normal SFXes
 ;	| (Default value is S&K's)
 ;
 SMPS_IdlingSegaSound	= 1
@@ -89,15 +81,16 @@ SMPS_EnableUniversalVoiceBank	= 0
 SMPS_SegaPCM_68k	= 0
 ;	| If 0, the Z80 and Mega PCM handle the SEGA sample playback. If 1, the 68k handles it. I recommend Z80, as with the 68k version, if overclocked, the sound plays wrongly.
 ;
-SMPS_S1DACSamples = 1
-SMPS_S2DACSamples = 0
-SMPS_S3DACSamples = 0
-SMPS_SKDACSamples = 0
-SMPS_S3DDACSamples = 0
-SMPS_SCDACSamples = 0
 
-SMPS_S1PSGEnvelopes = 1
-SMPS_S2PSGEnvelopes = 0
-SMPS_S3PSGEnvelopes = 0
-SMPS_SKPSGEnvelopes = 0
-SMPS_S3DPSGEnvelopes = 0
+SMPS_S1DACSamples	= 1
+SMPS_S2DACSamples	= 0
+SMPS_S3DACSamples	= 0
+SMPS_SKDACSamples	= 0
+SMPS_S3DDACSamples	= 0
+SMPS_SCDACSamples	= 0
+
+SMPS_S1PSGEnvelopes	= 1
+SMPS_S2PSGEnvelopes	= 0
+SMPS_S3PSGEnvelopes	= 0
+SMPS_SKPSGEnvelopes	= 0
+SMPS_S3DPSGEnvelopes	= 0
--- Sonic 2 files/Sonic 2 Clone Driver v2 - Compatibility.asm
+++ Sonic 2 files/Sonic 2 Clone Driver v2 - Compatibility.asm
@@ -1,13 +1,15 @@
 ; ========SONIC 2 GIT EQUATES========
 
-JmpTo_SoundDriverLoad =	SMPS_LoadDACDriver
-SoundDriverLoad =	SMPS_LoadDACDriver
+; ---DISASM-DEPENDANT VARIABLES AND FUNCTIONS---
+JmpTo_SoundDriverLoad	= SMPS_LoadDACDriver
+SoundDriverLoad		= SMPS_LoadDACDriver
 
-PlayMusic =		SMPS_PlayMusic
-PlaySoundLocal =	SMPS_PlaySoundLocal
-PlaySound =		SMPS_PlaySound
-PlaySoundStereo =	SMPS_PlaySound2
+PlayMusic		= SMPS_PlayMusic
+PlaySoundLocal		= SMPS_PlaySoundLocal
+PlaySound		= SMPS_PlaySound
+PlaySoundStereo		= SMPS_PlaySound2
 
+; ---FLAGS---
 SMPS_GloopSFXBehaviour	= 1
 ;	| If 1, SndID_Gloop only plays on every other call.
 ;
@@ -17,14 +19,14 @@ SMPS_PushSFXBehaviour	= 0
 SMPS_EnableSpecSFX	= 0
 ;	| If 1, include the Special SFX system. Used by S1's waterfall SFX
 ;
-SMPS_EnableSpinDashSFX = 1
+SMPS_EnableSpinDashSFX	= 1
 ;	| If 1, include S2's spin dash SFX pitch system
 ;
 SMPS_EnableContSFX	= 0
 ;	| If 1, include S3K's continuous SFX system
 ;
 SMPS_First_ContSFX	= $BC
-;	| Set this to the sound ID of your first continous SFX. This ID must be a higher number than any of your normal SFXes
+;	| Set this to the sound ID of your first continuous SFX. This ID must be a higher number than any of your normal SFXes
 ;	| (Default value is S&K's)
 ;
 SMPS_IdlingSegaSound	= 0
@@ -36,15 +38,16 @@ SMPS_EnableUniversalVoiceBank	= 0
 SMPS_SegaPCM_68k	= 0
 ;	| If 0, the Z80 and Mega PCM handle the SEGA sample playback. If 1, the 68k handles it. I recommend Z80, as with the 68k version, if overclocked, the sound plays wrongly.
 ;
-SMPS_S1DACSamples = 0
-SMPS_S2DACSamples = 1
-SMPS_S3DACSamples = 0
-SMPS_SKDACSamples = 0
-SMPS_S3DDACSamples = 0
-SMPS_SCDACSamples = 0
 
-SMPS_S1PSGEnvelopes = 0
-SMPS_S2PSGEnvelopes = 1
-SMPS_S3PSGEnvelopes = 0
-SMPS_SKPSGEnvelopes = 0
-SMPS_S3DPSGEnvelopes = 0
+SMPS_S1DACSamples	= 0
+SMPS_S2DACSamples	= 1
+SMPS_S3DACSamples	= 0
+SMPS_SKDACSamples	= 0
+SMPS_S3DDACSamples	= 0
+SMPS_SCDACSamples	= 0
+
+SMPS_S1PSGEnvelopes	= 0
+SMPS_S2PSGEnvelopes	= 1
+SMPS_S3PSGEnvelopes	= 0
+SMPS_SKPSGEnvelopes	= 0
+SMPS_S3DPSGEnvelopes	= 0
