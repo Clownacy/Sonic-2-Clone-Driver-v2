68ef80244530b72901e6dde78933a50616e2707a
Clownacy <Clownacy@users.noreply.github.com>
2016-08-01 21:52:12 +0100

    Improved Stub 32X build script

--- Stub 32X/build.bat
+++ Stub 32X/build.bat
@@ -1,7 +1,26 @@
 @echo off
-REM asmsh /q /p /o i+ /o psh2 /o w- "code.asm","code.bin", ,"out.out"
+
 set AS_MSGPATH=../../AS/Win32
 set USEANSI=n
 "../../AS/Win32/asw.exe" -xx -q -E -A -L code.asm
+
+IF NOT EXIST code.p goto LABELERROR
+
 set AS_MSGPATH=p2bin
 "p2bin/p2bin.exe" code.p code.bin -r 0x-0x
+
+IF EXIST code.log goto LABELWARNING
+
+del code.p
+
+exit /b
+
+:LABELERROR
+echo There were build errors. Check code.log.
+pause
+exit /b
+
+:LABELWARNING
+echo There were build warnings. Check code.log.
+pause
+exit /b
