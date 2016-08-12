6bcc8ea8c16ff2bdb8a03cca57d5f1c7cf16984c
Clownacy <Clownacy@users.noreply.github.com>
2016-08-12 15:58:07 +0100

    Added SMPS_PlayPWMSample

--- Sonic 2 Clone Driver v2 - Functions.asm
+++ Sonic 2 Clone Driver v2 - Functions.asm
@@ -63,11 +63,34 @@ SMPS_QueueSound3:
 
 ; ---------------------------------------------------------------------------
 ; Play a DAC sample
+;
+; d0 = Sample ID
 ; ---------------------------------------------------------------------------
-SMPS_PlaySample:
+SMPS_PlayDACSample:
 	SMPS_stopZ80_safe
 	st.b	(SMPS_z80_ram+MegaPCM_DAC_Type).l	; This is a DAC SFX; ignore music DAC volume
 	move.b  d0,(SMPS_z80_ram+MegaPCM_DAC_Number).l
 	SMPS_startZ80_safe
 	rts
-; End of function SMPS_PlaySample
+; End of function SMPS_PlayDACSample
+
+; ---------------------------------------------------------------------------
+; Play a PWM sample
+;
+; d0 = Sample ID
+; d1 = Sample volume/panning
+; d2 = PWM channel*2 (0 = channel 1, 2 = channel 2, etc.)
+; ---------------------------------------------------------------------------
+SMPS_PlayPWMSample:
+	; Merge ID with volume/pan to get PWM command
+	lsl.w	#8,d1
+	move.b	d0,d1
+	; Save a0
+	move.l	a0,d0
+	; Send PWM command
+	lea	($A15128).l,a0
+	move.w	d1,(a0,d2.w)
+	; Restore a0
+	movea.l	d0,a0
+	rts
+; End of function SMPS_PlayPWMSample
