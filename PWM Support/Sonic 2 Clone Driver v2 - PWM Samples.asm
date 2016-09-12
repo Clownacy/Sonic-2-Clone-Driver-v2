a1d9bf8886187b616193222b2afe7d8711d90488
Clownacy <Clownacy@users.noreply.github.com>
2016-09-12 17:57:52 +0100

    Moved PWM files into its own folder
    User is supposed to extract the contents on top of the driver. This
    eliminates the user needed to delete these individually as part of the
    guide.

---
+++ PWM Support/Sonic 2 Clone Driver v2 - PWM Samples.asm
@@ -0,0 +1,75 @@
+SH2_PWMTable:
+	PWMSampleEntry	PWM_00, $00000000, $00000800
+	PWMSampleEntry	PWM_01, $00000000, $00000800
+	PWMSampleEntry	PWM_02, $00000000, $00000800
+	PWMSampleEntry	PWM_03, $00000000, $00000800
+	PWMSampleEntry	PWM_04, $00000000, $00000800
+	PWMSampleEntry	PWM_04, $00000000, $00000659
+	PWMSampleEntry	PWM_04, $00000000, $000004C2
+	PWMSampleEntry	PWM_07, $00000000, $00000800
+	PWMSampleEntry	PWM_08, $00000000, $00000800
+	PWMSampleEntry	PWM_09, $00000000, $00000800
+	PWMSampleEntry	PWM_0A, $00000000, $00000800
+	PWMSampleEntry	PWM_0B, $00000000, $00000800
+	PWMSampleEntry	PWM_0C, $00000000, $00000800
+	PWMSampleEntry	PWM_0D, $00000000, $00000800
+	PWMSampleEntry	PWM_0E, $00000000, $00000800
+	PWMSampleEntry	PWM_0F, $00000000, $00000800
+	PWMSampleEntry	PWM_0F, $00000000, $000005FE
+	PWMSampleEntry	PWM_11, $00000000, $00000800
+	PWMSampleEntry	PWM_11, $00000000, $000005FE
+	PWMSampleEntry	PWM_13, $00000000, $00000800
+	PWMSampleEntry	PWM_13, $00000000, $000005FE
+	PWMSampleEntry	PWM_15, $00000000, $00000800
+
+PWM_00:		binclude	"sound/PWM/PWM_00.bin"
+PWM_00_End
+
+PWM_01:		binclude	"sound/PWM/PWM_01.bin"
+PWM_01_End
+
+PWM_02:		binclude	"sound/PWM/PWM_02.bin"
+PWM_02_End
+
+PWM_03:		binclude	"sound/PWM/PWM_03.bin"
+PWM_03_End
+
+PWM_04:		binclude	"sound/PWM/PWM_04.bin"
+PWM_04_End
+
+PWM_07:		binclude	"sound/PWM/PWM_07.bin"
+PWM_07_End
+
+PWM_08:		binclude	"sound/PWM/PWM_08.bin"
+PWM_08_End
+
+PWM_09:		binclude	"sound/PWM/PWM_09.bin"
+PWM_09_End
+
+PWM_0A:		binclude	"sound/PWM/PWM_0A.bin"
+PWM_0A_End
+
+PWM_0B:		binclude	"sound/PWM/PWM_0B.bin"
+PWM_0B_End
+
+PWM_0C:		binclude	"sound/PWM/PWM_0C.bin"
+PWM_0C_End
+
+PWM_0D:		binclude	"sound/PWM/PWM_0D.bin"
+PWM_0D_End
+
+PWM_0E:		binclude	"sound/PWM/PWM_0E.bin"
+PWM_0E_End
+
+PWM_0F:		binclude	"sound/PWM/PWM_0F.bin"
+PWM_0F_End
+
+PWM_11:		binclude	"sound/PWM/PWM_11.bin"
+PWM_11_End
+
+PWM_13:		binclude	"sound/PWM/PWM_13.bin"
+PWM_13_End
+
+PWM_15:		binclude	"sound/PWM/PWM_15.bin"
+PWM_15_End
+		even
