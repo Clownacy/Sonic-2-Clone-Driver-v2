1e49ed4bf934685115b2ba97067cde087eb4bb7e
Clownacy <Clownacy@users.noreply.github.com>
2016-10-02 15:09:19 +0100

    Don't "resume" the DAC channel
    It doesn't use an FM voice

--- engine/Sonic 2 Clone Driver v2.asm
+++ engine/Sonic 2 Clone Driver v2.asm
@@ -513,17 +513,20 @@ DoPauseMusic:
 DoUnpauseMusic:
 	clr.b	SMPS_RAM.f_stopmusic(a6)
 
-	lea	SMPS_RAM.v_music_fmdac_tracks(a6),a5
-	moveq	#SMPS_MUSIC_FM_DAC_TRACK_COUNT-1,d7	; 6 FM + 1 DAC
+	; Resume music FM channels
+	lea	SMPS_RAM.v_music_fm_tracks(a6),a5
+	moveq	#SMPS_MUSIC_FM_TRACK_COUNT-1,d7		; 6 FM
 	bsr.s	ResumeTrack
 
-	move.b	#$80,SMPS_RAM.f_voice_selector(a6)			; Now at SFX tracks
+	; Resume SFX FM channels
+	move.b	#$80,SMPS_RAM.f_voice_selector(a6)
 	lea	SMPS_RAM.v_sfx_fm_tracks(a6),a5
-	moveq	#SMPS_SFX_FM_TRACK_COUNT-1,d7	; 3 FM
+	moveq	#SMPS_SFX_FM_TRACK_COUNT-1,d7		; 3 FM
 	bsr.s	ResumeTrack
 
     if SMPS_EnableSpecSFX
-	move.b	#$40,SMPS_RAM.f_voice_selector(a6)			; Now at SFX tracks
+	; Resume Special SFX FM channels
+	move.b	#$40,SMPS_RAM.f_voice_selector(a6)
 	lea	SMPS_RAM.v_spcsfx_fm_tracks(a6),a5
 	moveq	#SMPS_SPECIAL_SFX_FM_TRACK_COUNT-1,d7	; 1 FM
 	bsr.s	ResumeTrack
@@ -546,10 +549,10 @@ locret_71EFE:
 ; ||||||||||||||| S U B	R O U T	I N E |||||||||||||||||||||||||||||||||||||||
 
 ResumeTrack:
-	tst.b	SMPS_Track.PlaybackControl(a5)	; Is track playing?
-	bpl.s	+				; Branch if not
+	tst.b	SMPS_Track.PlaybackControl(a5)		; Is track playing?
+	bpl.s	+					; Branch if not
 	btst	#2,SMPS_Track.PlaybackControl(a5)	; Is SFX overriding track?
-	bne.s	+				; Branch if not
+	bne.s	+					; Branch if not
 	moveq	#0,d0
 	move.b	SMPS_Track.VoiceIndex(a5),d0	; Current track FM instrument
 	bsr.w	cfSetVoiceCont
