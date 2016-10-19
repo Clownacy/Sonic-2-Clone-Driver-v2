43ef5c928ddf1cb42c4188827a11bf7692aa2147
Clownacy <Clownacy@users.noreply.github.com>
2016-10-19 23:38:36 +0100

    Changed Pitch parameter in DAC_Entry from djnz loop counter to playback rate (in Hz)
    Now, I can use the macro to auto-adjust the pitches whenever I optimise
    the DAC loop.
    
    Also, this should make things way easier for the user.

--- engine/MegaPCM - Z80.asm
+++ engine/MegaPCM - Z80.asm
@@ -612,7 +612,11 @@ zmake68kBank function addr,(((addr&3F8000h)/8000h))
 
 DAC_Entry macro vPitch,vOffset,vFlags
 	db	MegaPCM_vFlags			; 00h	- Flags
-	db	vPitch				; 01h	- Pitch
+	if MegaPCM_vFlags&MegaPCM_dpcm
+		db	((((3579545*2)/vPitch)-238)/26)+1	; 01h	- Pitch
+	else
+		db	(((3579545/vPitch)-130)/13)+1	; 01h	- Pitch
+	endif
 	db	zmake68kBank(vOffset)		; 02h	- Start Bank
 	db	zmake68kBank(vOffset_End)	; 03h	- End Bank
 	dw	zmake68kPtr(vOffset)		; 04h	- Start Offset (in Start bank)
