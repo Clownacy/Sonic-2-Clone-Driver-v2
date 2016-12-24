31b7497a8da9166cce6c1e0fd09b72389bc2890f
Clownacy <Clownacy@users.noreply.github.com>
2016-12-24 22:07:18 +0000

    Renamed PWM sample files

--- extra/PWM Support/Sonic 2 Clone Driver v2 - PWM Samples.asm
+++ extra/PWM Support/Sonic 2 Clone Driver v2 - PWM Samples.asm
@@ -22,54 +22,54 @@ ptr_pwm14:	PWMSampleEntry	PWM_13, $00000000, $00000800
 ptr_pwm15:	PWMSampleEntry	PWM_13, $00000000, $000005FE
 ptr_pwm16:	PWMSampleEntry	PWM_15, $00000000, $00000800
 
-PWM_00:		binclude	"sound/PWM/PWM_00.bin"
+PWM_00:		binclude	"sound/PWM/ElectricKick.bin"
 PWM_00_End
 
-PWM_01:		binclude	"sound/PWM/PWM_01.bin"
+PWM_01:		binclude	"sound/PWM/ElectricSnare.bin"
 PWM_01_End
 
-PWM_02:		binclude	"sound/PWM/PWM_02.bin"
+PWM_02:		binclude	"sound/PWM/SplashCymbal.bin"
 PWM_02_End
 
-PWM_03:		binclude	"sound/PWM/PWM_03.bin"
+PWM_03:		binclude	"sound/PWM/RideBell.bin"
 PWM_03_End
 
-PWM_04:		binclude	"sound/PWM/PWM_04.bin"
+PWM_04:		binclude	"sound/PWM/Tom.bin"
 PWM_04_End
 
-PWM_07:		binclude	"sound/PWM/PWM_07.bin"
+PWM_07:		binclude	"sound/PWM/ElectricHiHat.bin"
 PWM_07_End
 
-PWM_08:		binclude	"sound/PWM/PWM_08.bin"
+PWM_08:		binclude	"sound/PWM/CrashCymbal.bin"
 PWM_08_End
 
-PWM_09:		binclude	"sound/PWM/PWM_09.bin"
+PWM_09:		binclude	"sound/PWM/Clap.bin"
 PWM_09_End
 
-PWM_0A:		binclude	"sound/PWM/PWM_0A.bin"
+PWM_0A:		binclude	"sound/PWM/AcousticKick.bin"
 PWM_0A_End
 
-PWM_0B:		binclude	"sound/PWM/PWM_0B.bin"
+PWM_0B:		binclude	"sound/PWM/AcousticSnare.bin"
 PWM_0B_End
 
-PWM_0C:		binclude	"sound/PWM/PWM_0C.bin"
+PWM_0C:		binclude	"sound/PWM/Bell.bin"
 PWM_0C_End
 
-PWM_0D:		binclude	"sound/PWM/PWM_0D.bin"
+PWM_0D:		binclude	"sound/PWM/FingersSnapping.bin"
 PWM_0D_End
 
-PWM_0E:		binclude	"sound/PWM/PWM_0E.bin"
+PWM_0E:		binclude	"sound/PWM/Cowbell.bin"
 PWM_0E_End
 
-PWM_0F:		binclude	"sound/PWM/PWM_0F.bin"
+PWM_0F:		binclude	"sound/PWM/Click.bin"
 PWM_0F_End
 
-PWM_11:		binclude	"sound/PWM/PWM_11.bin"
+PWM_11:		binclude	"sound/PWM/Bongo.bin"
 PWM_11_End
 
-PWM_13:		binclude	"sound/PWM/PWM_13.bin"
+PWM_13:		binclude	"sound/PWM/Timpani.bin"
 PWM_13_End
 
-PWM_15:		binclude	"sound/PWM/PWM_15.bin"
+PWM_15:		binclude	"sound/PWM/Silence.bin"
 PWM_15_End
 		even
