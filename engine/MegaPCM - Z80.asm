Clownacy/Sonic-2-Clone-Driver-v2

commit 9e73c402eb5a8027e655a24fa59ea5ab531f963c

Optimised Mega PCM DPCM loop

engine/MegaPCM - Z80.asm
@@ -516,7 +516,9 @@ MegaPCM_Process_PCM_idle:
 ;	C	= Pitch
 ;	DE	= DPCM byte pointer
 ;	HL	= Delta Table
+;	BC'	= YM2612 data port pointer
 ;	DE'	= Volume byte pointer
+;	HL'	= Number of bytes in last bank
 ; ---------------------------------------------------------------
 
 ; ---------------------------------------------------------------
@@ -535,6 +537,9 @@ MegaPCM_Init_DPCM:
 	ld	(iy+0),2Ah		; YM => prepare to fetch DAC bytes
 	ld	b,80h			; init DAC value
 	ld	h,MegaPCM_DPCM_LowNibble>>8	; load delta table base
+	exx
+	ld	bc,MegaPCM_YM_Port0_Data
+	exx
 
 MegaPCM_Process_DPCM:
 
@@ -550,9 +555,9 @@ MegaPCM_Process_DPCM:
 	exx				; 4
 	ld	e,a			; 4	; Clownacy | get address of volume-adjusted PCM byte
 	ld	a,(de)			; 7	; Clownacy | get volume-adjusted PCM byte
+	ld	(bc),a			; 7	; write to DAC
 	exx				; 4
-	ld	(MegaPCM_YM_Port0_Data),a	; 13	; write to DAC
-	; Cycles: 74
+	; Cycles: 68
 
 	ld	a,(de)			; 7	; reload DPCM stream byte
 	dec	h			; 4	; load DPLC low nibble delta table base
@@ -565,9 +570,9 @@ MegaPCM_Process_DPCM:
 	exx				; 4
 	ld	e,a			; 4	; Clownacy | get address of volume-adjusted PCM byte
 	ld	a,(de)			; 7	; Clownacy | get volume-adjusted PCM byte
+	ld	(bc),a			; 7	; write to DAC
 	exx				; 4
-	ld	(MegaPCM_YM_Port0_Data),a	; 13	; write to DAC
-	; Cycles: 74
+	; Cycles: 68
 
 	; Increment DPCM byte pointer and switch the bank if necessary
 	inc	de			; 6	; next DPCM byte
@@ -602,8 +607,8 @@ MegaPCM_Process_DPCM_idle:
 	jp	-
 
 ; ---------------------------------------------------------------
-; Best cycles per loop:	224/2
-; Max possible rate:	3,579.545 kHz / 112 = 32 kHz (NTSC)
+; Best cycles per loop:	212/2
+; Max possible rate:	3,579.545 kHz / 106 = 34 kHz (NTSC)
 ; ---------------------------------------------------------------
 
 MegaPCM_LastBankNotReached:

extra/Sonic 2 files/Sonic 2 Clone Driver v2 - Compatibility.asm
@@ -1,6 +1,6 @@
 ; ========SONIC 2 GIT EQUATES========
 
-Size_of_Mega_PCM_guess = $260
+Size_of_Mega_PCM_guess = $28C
 
 ; ---FLAGS---
 SMPS_GloopSFXBehaviour	= 1

commit c9dbac27fd0597f4d0ce131fea3bd605a733efe8

Fixed multi-bank DPCM samples
Holy cow this has been broken since
9e73c402eb5a8027e655a24fa59ea5ab531f963c. That was almost a year ago.

engine/MegaPCM - Z80.asm
@@ -437,6 +437,9 @@ MegaPCM_LoadBank:
 ;	C	= Pitch
 ;	DE	= Volume byte pointer
 ;	HL	= PCM byte pointer
+;	B'	= Current bank
+;	C'	= Final bank
+;	HL'	= Number of bytes in last bank
 ; ---------------------------------------------------------------
 
 ; ---------------------------------------------------------------
@@ -516,7 +519,8 @@ MegaPCM_Process_PCM_idle:
 ;	C	= Pitch
 ;	DE	= DPCM byte pointer
 ;	HL	= Delta Table
-;	BC'	= YM2612 data port pointer
+;	B'	= Current bank
+;	C'	= Final bank
 ;	DE'	= Volume byte pointer
 ;	HL'	= Number of bytes in last bank
 ; ---------------------------------------------------------------
@@ -537,9 +541,6 @@ MegaPCM_Init_DPCM:
 	ld	(iy+0),2Ah		; YM => prepare to fetch DAC bytes
 	ld	b,80h			; init DAC value
 	ld	h,MegaPCM_DPCM_LowNibble>>8	; load delta table base
-	exx
-	ld	bc,MegaPCM_YM_Port0_Data
-	exx
 
 MegaPCM_Process_DPCM:
 
@@ -555,9 +556,9 @@ MegaPCM_Process_DPCM:
 	exx				; 4
 	ld	e,a			; 4	; Clownacy | get address of volume-adjusted PCM byte
 	ld	a,(de)			; 7	; Clownacy | get volume-adjusted PCM byte
-	ld	(bc),a			; 7	; write to DAC
+	ld	(MegaPCM_YM_Port0_Data),a	; 13	; write to DAC
 	exx				; 4
-	; Cycles: 68
+	; Cycles: 74
 
 	dec	h			; 4	; load DPLC low nibble delta table base
 	ld	a,b			; 4	; load DAC Value
@@ -568,9 +569,9 @@ MegaPCM_Process_DPCM:
 	exx				; 4
 	ld	e,a			; 4	; Clownacy | get address of volume-adjusted PCM byte
 	ld	a,(de)			; 7	; Clownacy | get volume-adjusted PCM byte
-	ld	(bc),a			; 7	; write to DAC
+	ld	(MegaPCM_YM_Port0_Data),a	; 13	; write to DAC
 	exx				; 4
-	; Cycles: 57
+	; Cycles: 63
 
 	; Increment DPCM byte pointer and switch the bank if necessary
 	inc	de			; 6	; next DPCM byte
@@ -605,8 +606,8 @@ MegaPCM_Process_DPCM_idle:
 	jp	-
 
 ; ---------------------------------------------------------------
-; Best cycles per loop:	201/2
-; Max possible rate:	3,579.545 kHz / 100.5 = 35.6 kHz (NTSC)
+; Best cycles per loop:	213/2
+; Max possible rate:	3,579.545 kHz / 106.5 = 33.6 kHz (NTSC)
 ; ---------------------------------------------------------------
 
 MegaPCM_LastBankNotReached:
@@ -666,7 +667,7 @@ DAC_Entry macro vPitch,vOffset,vFlags
 	; the '*10's and '+5'.
 
 	if vFlags&MegaPCM_dpcm
-		db	(((((((3579545*10)*2)/vPitch)-(201*10))/(13*2))+5)/10)+1	; 01h	- Pitch (DPCM-converted)
+		db	(((((((3579545*10)*2)/vPitch)-(213*10))/(13*2))+5)/10)+1	; 01h	- Pitch (DPCM-converted)
 	else
 		db	((((((3579545*10)/vPitch)-(115*10))/13)+5)/10)+1		; 01h	- Pitch (PCM-converted)
 	endif