8637aff7cf94b4b8c999ce6b421eaa03b08bc0cb
Clownacy <Clownacy@users.noreply.github.com>
2021-04-23 22:26:03 +0100

    Another Sega PCM fix

--- engine/Sonic 2 Clone Driver v2.asm
+++ engine/Sonic 2 Clone Driver v2.asm
@@ -635,8 +635,14 @@ ptr_flgend
 ; ---------------------------------------------------------------------------
 ; Sound_E1: PlaySega:
 PlaySegaSound:
-	move.b	#$B6,d0		; Register for FM6/DAC AMS/FMS/Panning
-	move.b	#$C0,d1		; Left and right
+	; Enable DAC
+	moveq	#$2B,d0		; Register for FM6/DAC AMS/FMS/Panning
+	move.b	#$80,d1		; Left and right
+	bsr.w	WriteFMI
+
+	; Force L/R panning
+	move.b	#$B6,d0
+	move.b	#$C0,d1
 	bsr.w	WriteFMII
 
 	moveq	#(MegaPCM_VolumeTbls&$F000)>>8,d0
