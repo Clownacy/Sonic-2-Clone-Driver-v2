Clownacy/Sonic-2-Clone-Driver-v2

commit 7ef65f2e887ff2958c272538603a6bd176336192

Fix DAC track lagging behind after 1UP

engine/Sonic 2 Clone Driver v2.asm
@@ -60,6 +60,7 @@ SMPS_UpdateDriver:
 	bsr.s	UpdateMusic
 
 ;.updatesfxtracks:
+	lea	SMPS_RAM.v_sfx_track_ram(a6),a5
 	moveq	#SMPS_SFX_FM_TRACK_COUNT-1,d7	; SFX only has access to 3 FM tracks
 ; loc_71C04:
 .sfxfmloop:

commit a7fe0fc30355e89a825ea634111dc8048a828679

Fix broken SFX
Was caused by 7ef65f2e887ff2958c272538603a6bd176336192

engine/Sonic 2 Clone Driver v2.asm
@@ -64,27 +64,26 @@ SMPS_UpdateDriver:
 	moveq	#SMPS_SFX_FM_TRACK_COUNT-1,d7	; SFX only has access to 3 FM tracks
 ; loc_71C04:
 .sfxfmloop:
-	lea	SMPS_Track.len(a5),a5
 	tst.b	SMPS_Track.PlaybackControl(a5)	; Is track playing?
 	bpl.s	.sfxfmnext			; Branch if not
 	bsr.w	FMUpdateTrack
 ; loc_71C10:
 .sfxfmnext:
+	lea	SMPS_Track.len(a5),a5
 	dbf	d7,.sfxfmloop
 
 	moveq	#SMPS_SFX_PSG_TRACK_COUNT-1,d7	; SFX only has access to 3 PSG tracks
 ; loc_71C16:
 .sfxpsgloop:
-	lea	SMPS_Track.len(a5),a5
 	tst.b	SMPS_Track.PlaybackControl(a5)	; Is track playing?
 	bpl.s	.sfxpsgnext			; Branch of not
 	bsr.w	PSGUpdateTrack
 ; loc_71C22:
 .sfxpsgnext:
+	lea	SMPS_Track.len(a5),a5
 	dbf	d7,.sfxpsgloop
 
     if SMPS_EnableSpecSFX
-	lea	SMPS_Track.len(a5),a5
 	tst.b	SMPS_Track.PlaybackControl(a5)	; Is track playing?
 	bpl.s	.specfmdone			; Branch if not
 	bsr.w	FMUpdateTrack